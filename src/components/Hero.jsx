69329215d408b85c4e49e912db54c31e119897a5
Naveen-pusa <naveenpusa2000@gmail.com>
2026-04-27 19:07:49 +0530

    MovieNames

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <>
+     <div style={{ padding: "50px", background: "#000", color: "white" }}>
+      <h1>Discover Movies Worth Watching</h1>
+      <p>Search, explore, and get complete movie details instantly.</p>
+    </div>
+    </>
+  )
+}
+
+export default Hero
